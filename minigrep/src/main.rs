3a3a2b344e5476485e80d2919c6d640dcb914548
ervin.centeno <ecent416@gmail.com>
2026-05-24 21:12:34 -0400

    Updated read function to output error messages to a file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,15 +11,13 @@ fn main() {
     let args: Vec<String> = env::args().collect(); // Command line input
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguements: {}", err);
+        eprintln!("Problem parsing arguements: {}", err); // Error message print
         process::exit(1);
     });
 
-    println!("Searching for '{}'\n", config.query);
-
     // Handle the error condition for read_file
     if let Err(e) = read_file(&config) {
-        println!("Application error: {}", e); 
+        eprintln!("Application error: {}", e); // Error message print
         process::exit(1);
     };
 
